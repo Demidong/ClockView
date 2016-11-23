62f12bf6d863ef691bb2eb988ee9bf0301970c5c
qiuping.dong <qiuping.dong@baidao.com>
2016-11-23 15:06:12 +0800

    画透明圆

--- clockDemo/src/main/java/com/dx/demi/PieChatView.java
+++ clockDemo/src/main/java/com/dx/demi/PieChatView.java
@@ -4,11 +4,15 @@ import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
 
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -23,23 +27,77 @@ import java.util.Set;
  */
 
 public class PieChatView extends View {
+    private static final int DEFAULT_MINANGLE = 30;
     private int sum = 0;
+    private int minAngle = DEFAULT_MINANGLE;
     /**
      * 存放事物的品种与其对应的数量
      */
     private Map kindsMap = new LinkedHashMap<String, Integer>();
+
     private ArrayList<Integer> colors = new ArrayList<>();
 
     private Paint mPaint;//饼状画笔
     private Paint mTextPaint; // 文字画笔
     private static final int DEFAULT_RADIUS = 200;
-    private int mRadius = DEFAULT_RADIUS; //时钟的半径
+    private int mRadius = DEFAULT_RADIUS; //外园的半径
     private float animatedValue;
     private RectF oval;
+    private String centerTitle;
+
+    public int getMinAngle() {
+        return minAngle;
+    }
+
+    public void setMinAngle(int minAngle) {
+        this.minAngle = minAngle;
+    }
+
+    public int getmRadius() {
+        return mRadius;
+    }
+
+    public void setmRadius(int mRadius) {
+        this.mRadius = mRadius;
+    }
+
+    public ArrayList<Integer> getColors() {
+        return colors;
+    }
+
+    public void setColors(ArrayList<Integer> colors) {
+        this.colors = colors;
+    }
+
+    public Map getKindsMap() {
+        return kindsMap;
+    }
+
+    public void setKindsMap(Map kindsMap) {
+        this.kindsMap = kindsMap;
+    }
+
+    public String getCenterTitle() {
+        return centerTitle;
+    }
+
+    public void setCenterTitle(String centerTitle) {
+        this.centerTitle = centerTitle;
+    }
+
+    public float getAnimatedValue() {
+        return animatedValue;
+    }
+
+    public void setAnimatedValue(float animatedValue) {
+        this.animatedValue = animatedValue;
+    }
+
     /**
      * 格式化小数点
      */
     private DecimalFormat dff;
+
     public PieChatView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         mPaint = new Paint();
@@ -51,40 +109,9 @@ public class PieChatView extends View {
         mTextPaint.setColor(Color.BLACK);
         mTextPaint.setAntiAlias(true);
         mTextPaint.setStyle(Paint.Style.STROKE);
-        mTextPaint.setTextSize(15);
         mTextPaint.setTextAlign(Paint.Align.CENTER);
 
-        kindsMap.put("苹果", 10);
-        kindsMap.put("梨子", 30);
-        kindsMap.put("香蕉", 10);
-        kindsMap.put("葡萄", 30);
-        kindsMap.put("哈密瓜", 10);
-        kindsMap.put("猕猴桃",30);
-        kindsMap.put("草莓", 10);
-        kindsMap.put("橙子", 30);
-        kindsMap.put("火龙果", 10);
-        kindsMap.put("椰子", 20);
-        ValueAnimator anim = ValueAnimator.ofFloat(0, 360);
-        anim.setDuration(5000);
-        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-
-
-            @Override
-            public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                animatedValue = (float) valueAnimator.getAnimatedValue();
-                invalidate();
-            }
-        });
-        anim.start();
-        sum = getSum(kindsMap);
-        for(int i = 0; i<10; i++){
-            int random =  new Random().nextInt(100);
-            int  color =  Color.rgb(random + 5 * i,random * i,random - 5 * i);
-            if(!colors.contains(color)){
-                colors.add(color) ;
-            }
-        }
-        dff =new DecimalFormat("0.0");
+        dff = new DecimalFormat("0.0");
     }
 
     public PieChatView(Context context) {
@@ -132,68 +159,118 @@ public class PieChatView extends View {
     @Override
     protected void onDraw(Canvas mCanvas) {
         super.onDraw(mCanvas);
-        mCanvas.translate((getWidth()+ getPaddingLeft() - getPaddingRight()) / 2, (getHeight() + getPaddingTop() - getPaddingBottom())/ 2);
+        mCanvas.translate((getWidth() + getPaddingLeft() - getPaddingRight()) / 2, (getHeight() + getPaddingTop() - getPaddingBottom()) / 2);
 
-        paintPie(mCanvas, kindsMap);
+        paintPie(mCanvas);
 
     }
 
 
-    private void paintPie(final Canvas mCanvas, Map<String, Integer> map) {
-        Set<String> set = map.keySet();
-        Iterator<String> iterator = set.iterator();
-        int i = 0;
-        float currentAngle = 0.0f;
-        while (iterator.hasNext()) {
-            String kinds = iterator.next();
-            int num = map.get(kinds);
-            float needDrawAngle = num * 1.0f / sum * 360;
-            String drawAngle = dff.format(needDrawAngle/360*100);
-            kinds =kinds +","+drawAngle+"%";
-            float textAngle = needDrawAngle / 2 + currentAngle;
-            if (animatedValue - currentAngle > 0) {
-                mPaint.setColor(colors.get(i));
-                mCanvas.drawArc(oval, currentAngle, animatedValue - currentAngle, true, mPaint);
-                System.out.println("draw---->currentAngle: " + currentAngle + "needDrawAngle:" + needDrawAngle + "animatedValue - currentAngle:" + (animatedValue - currentAngle));
+    private void paintPie(final Canvas mCanvas) {
+        if (kindsMap != null && centerTitle != null) {
+            Set<Map.Entry<String, Integer>> entrySet = kindsMap.entrySet();
+            Iterator<Map.Entry<String, Integer>> iterator = entrySet.iterator();
+            int i = 0;
+            float currentAngle = 0.0f;
+            while (iterator.hasNext()) {
+                Map.Entry<String, Integer> entry = iterator.next();
+                String kinds = entry.getKey();
+                int num = entry.getValue();
+                float needDrawAngle = num * 1.0f / sum * 360;
+                String drawAngle = dff.format(needDrawAngle / 360 * 100);
+                kinds = kinds + "," + drawAngle + "%";
+                float textAngle = needDrawAngle / 2 + currentAngle;
+                if (Math.min(needDrawAngle, animatedValue - currentAngle) >= 0) {
+                    mPaint.setColor(colors.get(i));
+                    mCanvas.drawArc(oval, currentAngle, Math.min(needDrawAngle - 1, animatedValue - currentAngle), true, mPaint);
 
-                drawText(mCanvas,textAngle,kinds,needDrawAngle);
+                 //   System.out.println("draw---->currentAngle: " + currentAngle + "needDrawAngle:" + needDrawAngle + "animatedValue - currentAngle:" + (animatedValue - currentAngle));
+
+                    mPaint.setColor(Color.WHITE);
+                    mPaint.setAlpha(10);
+                    mCanvas.drawCircle(0, 0, mRadius / 2 + 20, mPaint);
+                    mPaint.setAlpha(255);
+                    mCanvas.drawCircle(0, 0, mRadius / 2, mPaint);
+                    drawText(mCanvas, textAngle, kinds, needDrawAngle);
+                    drawCenterText(mCanvas, centerTitle, 0, 0, mTextPaint);
+                }
+
+              //  System.out.println("currentAngle: " + currentAngle + "needDrawAngle:" + needDrawAngle + "animatedValue:" + animatedValue);
+                currentAngle = currentAngle + needDrawAngle;
+                i++;
             }
-            System.out.println("currentAngle: " + currentAngle + "needDrawAngle:" + needDrawAngle + "animatedValue:" + animatedValue);
-            currentAngle = currentAngle + needDrawAngle;
-            i++;
+        }
+
+
+    }
+
+    //画中间文字标题
+    private void drawCenterText(Canvas mCanvas, String text, float x, float y, Paint mPaint) {
+        Rect rect = new Rect();
+        mTextPaint.setTextSize(dp2px(20));
+        mTextPaint.getTextBounds(text, 0, text.length(), rect);
+        mCanvas.drawText(text, x, y+rect.height()/2, mTextPaint);
+    }
+
+    //画文字
+    private void drawText(Canvas mCanvas, float textAngle, String kinds, float needDrawAngle) {
+        Rect rect = new Rect();
+        mTextPaint.setTextSize(dp2px(15));
+        mTextPaint.getTextBounds(kinds, 0, kinds.length(), rect);
+        if (textAngle >= 0 && textAngle <= 90) { //画布坐标系第一象限(数学坐标系第四象限)
+            if (needDrawAngle < minAngle) { //如果小于某个度数,就把文字画在饼状图外面
+                mCanvas.drawText(kinds, (float) (mRadius * 1.2 * Math.cos(Math.toRadians(textAngle))), (float) (mRadius * 1.2 * Math.sin(Math.toRadians(textAngle)))+rect.height()/2, mTextPaint);
+            } else {
+                mCanvas.drawText(kinds, (float) (mRadius * 0.75 * Math.cos(Math.toRadians(textAngle))), (float) (mRadius * 0.75 * Math.sin(Math.toRadians(textAngle)))+rect.height()/2, mTextPaint);
+            }
+        } else if (textAngle > 90 && textAngle <= 180) { //画布坐标系第二象限(数学坐标系第三象限)
+            if (needDrawAngle < minAngle) {
+                mCanvas.drawText(kinds, (float) (-mRadius * 1.2 * Math.cos(Math.toRadians(180 - textAngle))), (float) (mRadius * 1.2 * Math.sin(Math.toRadians(180 - textAngle)))+rect.height()/2, mTextPaint);
+            } else {
+                mCanvas.drawText(kinds, (float) (-mRadius * 0.75 * Math.cos(Math.toRadians(180 - textAngle))), (float) (mRadius * 0.75 * Math.sin(Math.toRadians(180 - textAngle)))+rect.height()/2, mTextPaint);
+            }
+        } else if (textAngle > 180 && textAngle <= 270) { //画布坐标系第三象限(数学坐标系第二象限)
+            if (needDrawAngle < minAngle) {
+                mCanvas.drawText(kinds, (float) (-mRadius * 1.2 * Math.cos(Math.toRadians(textAngle - 180))), (float) (-mRadius * 1.2 * Math.sin(Math.toRadians(textAngle - 180)))+rect.height()/2, mTextPaint);
+            } else {
+                mCanvas.drawText(kinds, (float) (-mRadius * 0.75 * Math.cos(Math.toRadians(textAngle - 180))), (float) (-mRadius * 0.75 * Math.sin(Math.toRadians(textAngle - 180)))+rect.height()/2, mTextPaint);
+            }
+        } else { //画布坐标系第四象限(数学坐标系第一象限)
+            if (needDrawAngle < minAngle) {
+                mCanvas.drawText(kinds, (float) (mRadius * 1.2 * Math.cos(Math.toRadians(360 - textAngle))), (float) (-mRadius * 1.2 * Math.sin(Math.toRadians(360 - textAngle)))+rect.height()/2, mTextPaint);
+            } else {
+                mCanvas.drawText(kinds, (float) (mRadius * 0.75 * Math.cos(Math.toRadians(360 - textAngle))), (float) (-mRadius * 0.75 * Math.sin(Math.toRadians(360 - textAngle)))+rect.height()/2, mTextPaint);
+            }
+        }
+
+    }
+
+    public void setDataMap(LinkedHashMap<String, Integer> map) {
+        this.kindsMap = map;
+        sum = getSum(map);
+    }
+
+    public void startDraw() {
+        if (kindsMap != null && colors != null && centerTitle != null) {
+            initAnimator();
         }
     }
 
-   private void  drawText(Canvas mCanvas,float textAngle,String kinds,float needDrawAngle){
-       Rect rect = new Rect();
-       mTextPaint.getTextBounds(kinds, 0, kinds.length(), rect);
-       if (textAngle >= 0 && textAngle <= 90) {
-           if (needDrawAngle < 30) {
-               mCanvas.drawText(kinds, (float) (mRadius * 1.2 * Math.cos(Math.toRadians(textAngle))), (float) (mRadius * 1.2 * Math.sin(Math.toRadians(textAngle))), mTextPaint);
-           } else {
-               mCanvas.drawText(kinds, (float) (mRadius / 2 * Math.cos(Math.toRadians(textAngle))), (float) (mRadius / 2 * Math.sin(Math.toRadians(textAngle))), mTextPaint);
-           }
-       } else if (textAngle > 90 && textAngle <= 180) {
-           if (needDrawAngle < 30) {
-               mCanvas.drawText(kinds, (float) (-mRadius * 1.2 * Math.cos(Math.toRadians(180 -textAngle))), (float) (mRadius * 1.2 * Math.sin(Math.toRadians(180 -textAngle))), mTextPaint);
-           } else {
-               mCanvas.drawText(kinds, (float) (-mRadius / 2 * Math.cos(Math.toRadians(180 - textAngle))), (float) (mRadius / 2 * Math.sin(Math.toRadians(180 - textAngle))), mTextPaint);
-           }
-       } else if (textAngle > 180 && textAngle <= 270) {
-           if (needDrawAngle < 30) {
-               mCanvas.drawText(kinds, (float) (-mRadius * 1.2 * Math.cos(Math.toRadians(textAngle-180))), (float) (-mRadius * 1.2 * Math.sin(Math.toRadians(textAngle-180))), mTextPaint);
-           } else {
-               mCanvas.drawText(kinds, (float) (-mRadius / 2 * Math.cos(Math.toRadians(textAngle - 180))), (float) (-mRadius / 2 * Math.sin(Math.toRadians(textAngle - 180))), mTextPaint);
-           }
-       } else {
-           if (needDrawAngle < 30) {
-               mCanvas.drawText(kinds, (float) (mRadius * 1.2 * Math.cos(Math.toRadians(360-textAngle))), (float) (-mRadius * 1.2 * Math.sin(Math.toRadians(360-textAngle))), mTextPaint);
-           } else {
-               mCanvas.drawText(kinds, (float) (mRadius / 2 * Math.cos(Math.toRadians(360 - textAngle))), (float) (-mRadius / 2 * Math.sin(Math.toRadians(360 - textAngle))), mTextPaint);
-           }
-       }
+    private void initAnimator() {
+        ValueAnimator anim = ValueAnimator.ofFloat(0, 360);
+        anim.setDuration(10000);
+        anim.setInterpolator(new AccelerateDecelerateInterpolator());
+        anim.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 
-   }
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                animatedValue = (float) valueAnimator.getAnimatedValue();
+                invalidate();
+            }
+        });
+        anim.start();
+    }
+
     private int getSum(Map<String, Integer> map) {
         Set<String> set = map.keySet();
         Iterator<String> iterator = set.iterator();
@@ -206,5 +283,25 @@ public class PieChatView extends View {
         }
         return sum;
     }
+    /**
+     * dp 2 px
+     *
+     * @param dpVal
+     */
+    protected int dp2px(int dpVal) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
+                dpVal, getResources().getDisplayMetrics());
+    }
 
+    /**
+     * sp 2 px
+     *
+     * @param spVal
+     * @return
+     */
+    protected int sp2px(int spVal) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP,
+                spVal, getResources().getDisplayMetrics());
+
+    }
 }
